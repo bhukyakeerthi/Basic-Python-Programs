6d1510da618c6a0987cc8824a7c217ebc25878c8
bhukyakeerthi <238w1a0265@vrsec.ac.in>
2025-09-17 16:10:03 +0530

    Add files via upload

---
+++ keerti.py
@@ -0,0 +1,68 @@
+#Print get input from user and print it?
+'''
+num1=input("enter your first number")
+num2=input("enter your second number")
+print("First number:",num1)
+print("Second number:",num2)
+#Get the input from user add two numbers
+num3=int(input("Enter your third number"))
+num4=int(input("Enter your fourth number"))
+sum=num3+num4
+print("sum of two values:",sum)
+#First name & last name
+a=input("Enter the first name")
+b=input("Enter the last name")
+c=a+b
+print("your full name",c)
+#Write a python program get a input from user swap 2 numbers without 3rd variable
+a=10;
+b=20;
+a=a+b
+b=a-b
+a=a-b
+print("a:",a)
+print("b:",b)
+#with using 3rd variable
+a=10;
+b=20;
+temp=a
+a=b
+b=temp;
+print("after swapping:",a)
+print("after swapping:",b)
+#Write the python program to print the primitive data types
+a=10
+b=2.13
+c=True
+print("integer is:",a)
+print("float is:",b)
+print("boolean is:",c)
+#Write the python program to print the non-primitive data types
+a=(10,20,30)
+b=[10,10,20,30]
+c={10,20,20,30}
+dict_1={"One":1,"Two":2}
+print(a)
+print(b)
+print(c)
+print(dict_1)
+#Write the python program to square of a number
+a=int(input("Enter a number:"))
+b=a**2
+print(b)
+#Find the avg of 3 numbers you have to get input from user
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+average=(num1 + num2 + num3)/3
+print("The average is:", average)
+'''
+#Arimatic
+a=10
+b=20
+print("addition of two number:",a)
+print("subtraction of two numbers:",a-b)
+print("multiplication of two numbers:",a*b)
+print("division of two numbers:",a/b)
+
+
