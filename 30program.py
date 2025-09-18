5e713442d60314faea64f004dbe6bd48fa8de169
bhukyakeerthi <238w1a0265@vrsec.ac.in>
2025-09-18 12:25:02 +0530

    Add files via upload

---
+++ 30program.py
@@ -0,0 +1,344 @@
+#Write a python prograamm to print your name
+print("Keerthi")
+#Write ge the input from the user and printing
+name=input("Enter your name")
+print("your name:",name)
+#Print get input from user and print it?
+num1=input("enter your first number")
+num2=input("enter your second number")
+print("First number:",num1)
+print("Second number:",num2)
+#Get the input from user add two numbers
+num3=int(input("Enter your third number"))
+num4=int(input("Enter your fourth number"))
+sum=num3+num4
+print("sum of two values:",sum)
+#First name & last name
+a=input("Enter the first name")
+b=input("Enter the last name")
+c=a+b
+print("your full name",c)
+#Write a python program get a input from user swap 2 numbers without 3rd variable
+a=10;
+b=20;
+a=a+b
+b=a-b
+a=a-b
+print("a:",a)
+print("b:",b)
+#with using 3rd variable
+a=10;
+b=20;
+temp=a
+a=b
+b=temp;
+print("after swapping:",a)
+print("after swapping:",b)
+#Write the python program to print the primitive data types
+a=10
+b=2.13
+c=True
+print("integer is:",a)
+print("float is:",b)
+print("boolean is:",c)
+#Write the python program to print the non-primitive data types
+a=(10,20,30)
+b=[10,10,20,30]
+c={10,20,20,30}
+dict_1={"One":1,"Two":2}
+print(a)
+print(b)
+print(c)
+print(dict_1)
+#Write the python program to square of a number
+a=int(input("Enter a number:"))
+b=a**2
+print(b)
+#Find the avg of 3 numbers you have to get input from user
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+average=(num1 + num2 + num3)/3
+print("The average is:", average)
+#Arimatic
+a=10
+b=20
+print("addition of two number:",a)
+print("subtraction of two numbers:",a-b)
+print("multiplication of two numbers:",a*b)
+print("division of two numbers:",a/b)#Print get input from user and print it?
+num1=input("enter your first number")
+num2=input("enter your second number")
+print("First number:",num1)
+print("Second number:",num2)
+#Get the input from user add two numbers
+num3=int(input("Enter your third number"))
+num4=int(input("Enter your fourth number"))
+sum=num3+num4
+print("sum of two values:",sum)
+#First name & last name
+a=input("Enter the first name")
+b=input("Enter the last name")
+c=a+b
+print("your full name",c)
+#Write a python program get a input from user swap 2 numbers without 3rd variable
+a=10;
+b=20;
+a=a+b
+b=a-b
+a=a-b
+print("a:",a)
+print("b:",b)
+#with using 3rd variable
+a=10;
+b=20;
+temp=a
+a=b
+b=temp;
+print("after swapping:",a)
+print("after swapping:",b)
+#Write the python program to print the primitive data types
+a=10
+b=2.13
+c=True
+print("integer is:",a)
+print("float is:",b)
+print("boolean is:",c)
+#Write the python program to print the non-primitive data types
+a=(10,20,30)
+b=[10,10,20,30]
+c={10,20,20,30}
+dict_1={"One":1,"Two":2}
+print(a)
+print(b)
+print(c)
+print(dict_1)
+#Write the python program to square of a number
+a=int(input("Enter a number:"))
+b=a**2
+print(b)
+#Find the avg of 3 numbers you have to get input from user
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+average=(num1 + num2 + num3)/3
+print("The average is:", average)
+#Arimatic
+a=10
+b=20
+print("addition of two number:",a)
+print("subtraction of two numbers:",a-b)
+print("multiplication of two numbers:",a*b)
+print("division of two numbers:",a/b)#Print get input from user and print it?
+num1=input("enter your first number")
+num2=input("enter your second number")
+print("First number:",num1)
+print("Second number:",num2)
+#Get the input from user add two numbers
+num3=int(input("Enter your third number"))
+num4=int(input("Enter your fourth number"))
+sum=num3+num4
+print("sum of two values:",sum)
+#First name & last name
+a=input("Enter the first name")
+b=input("Enter the last name")
+c=a+b
+print("your full name",c)
+#Write a python program get a input from user swap 2 numbers without 3rd variable
+a=10;
+b=20;
+a=a+b
+b=a-b
+a=a-b
+print("a:",a)
+print("b:",b)
+#with using 3rd variable
+a=10;
+b=20;
+temp=a
+a=b
+b=temp;
+print("after swapping:",a)
+print("after swapping:",b)
+#Write the python program to print the primitive data types
+a=10
+b=2.13
+c=True
+print("integer is:",a)
+print("float is:",b)
+print("boolean is:",c)
+#Write the python program to print the non-primitive data types
+a=(10,20,30)
+b=[10,10,20,30]
+c={10,20,20,30}
+dict_1={"One":1,"Two":2}
+print(a)
+print(b)
+print(c)
+print(dict_1)
+#Write the python program to square of a number
+a=int(input("Enter a number:"))
+b=a**2
+print(b)
+#Find the avg of 3 numbers you have to get input from user
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+average=(num1 + num2 + num3)/3
+print("The average is:", average)
+#Arimatic
+a=10
+b=20
+print("addition of two number:",a)
+print("subtraction of two numbers:",a-b)
+print("multiplication of two numbers:",a*b)
+print("division of two numbers:",a/b)
+#Print get input from user and print it?
+num1=input("enter your first number")
+num2=input("enter your second number")
+print("First number:",num1)
+print("Second number:",num2)
+#Get the input from user add two numbers
+num3=int(input("Enter your third number"))
+num4=int(input("Enter your fourth number"))
+sum=num3+num4
+print("sum of two values:",sum)
+#First name & last name
+a=input("Enter the first name")
+b=input("Enter the last name")
+c=a+b
+print("your full name",c)
+#Write a python program get a input from user swap 2 numbers without 3rd variable
+a=10;
+b=20;
+a=a+b
+b=a-b
+a=a-b
+print("a:",a)
+print("b:",b)
+#with using 3rd variable
+a=10;
+b=20;
+temp=a
+a=b
+b=temp;
+print("after swapping:",a)
+print("after swapping:",b)
+#Write the python program to print the primitive data types
+a=10
+b=2.13
+c=True
+print("integer is:",a)
+print("float is:",b)
+print("boolean is:",c)
+#Write the python program to print the non-primitive data types
+a=(10,20,30)
+b=[10,10,20,30]
+c={10,20,20,30}
+dict_1={"One":1,"Two":2}
+print(a)
+print(b)
+print(c)
+print(dict_1)
+#Write the python program to square of a number
+a=int(input("Enter a number:"))
+b=a**2
+print(b)
+#Find the avg of 3 numbers you have to get input from user
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter the third number:"))
+average=(num1 + num2 + num3)/3
+print("The average is:", average)
+#Arimatic
+a=10
+b=20
+print("addition of two number:",a)
+print("subtraction of two numbers:",a-b)
+print("multiplication of two numbers:",a*b)
+print("division of two numbers:",a/b)
+#logical operators
+a=10
+b=12
+print(a>0 and b>15)
+print(a>15 or b<5)
+print(not(a==10))
+#write a python program to print positive and negitive
+a=int(input("Enter a number"))
+if(a>0):
+    print("positive")
+    if(a<0):
+        print("negative")
+    elif(a==0):
+        print("zero")
+    else:
+        print('error')
+#Write a python programm odd or even number
+a=int(input("Enter a number"))
+if(a%2==0):
+    print("number is even")
+else:
+    print("number is odd")
+#Write apython programm to check a number is divisible by 5 & 11
+a=int(input("Enter a number"))
+if(a%5==0 and a%11==0):
+    print("number is divisible by 5 and 11")
+else:
+    print("number is not divisible")
+#a students pass and exam marks
+theory_marks=int(input("Enter theroy marks:"))
+practical_marks=int(input("Enter practical marks:"))
+total_marks=theory_marks+practical_marks
+if theory_marks>=35 and practical_marks>=50 and total_marks>60:
+    print("Student passed")
+else:
+    print("Student failed")
+#Write a python programm to check the age group given by the user
+age=int(input("Enter your age"))
+if(age<13):
+    print("child")
+elif(age<=19):
+    print("Teenage")
+else:
+    print("Adult")
+#write a python programm to check the grade using marks
+marks=int(input("Enter your marks"))
+if(marks>=90):
+    print("grade is A & excellent")
+elif(marks>=70):
+    print("grade is B & very good")
+elif(marks>=50):
+    print("grade is C & Good")
+elif(marks>=40):
+    print("grade is D & better try")
+else:
+    print("Fail")
+#Write a python program to check the user name and password
+user="keerthi"
+pwd=222
+username=input("Enter you username")
+if username!=user:
+    print("invalid username")
+    username=input("Enter the username:")
+if username==user:
+    print("correctt username")
+    password=int(input("Enter the password"))
+if (username==user and password==pwd):
+    print("login sucess")
+else:
+    print("Enter valid password")
+    password=int(input("Enter the password:"))
+if (username==user and password==pwd):
+    print("login sucess")
+else:
+ print("Enter valid password")
+#Write a python progra to check the month days
+month=int(input("Enter the month 1-12"))
+if(month==2):
+    print("28 or 29 days")
+elif(month==1,3,5,7,8,10,12):
+    print("31 days")
+elif(month==4,6,9,11):
+    print("30 days")
+else:
+    print("invalid month")
